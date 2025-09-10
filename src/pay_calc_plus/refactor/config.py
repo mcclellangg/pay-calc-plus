e00b1e698d61937257efd551e828847579fbd2de
Grayson <mcclellangg@gmail.com>
2025-09-10 16:46:45 -0400

    remove unnescessary config value

--- src/pay_calc_plus/refactor/config.py
+++ src/pay_calc_plus/refactor/config.py
@@ -1,11 +1,5 @@
 TITLE = "Pay-Calc+"
 GEOMETRY = "750x450+250+250"
-FORM_FIELDS = {
-    "name": [
-        {"type": "label", "text": "Enter employee name: ", "anchor": "w", "width": 18},
-        {"type": "entry", "width": 40},
-    ]
-}
 
 WIDGETS = {
     "name_label": {
