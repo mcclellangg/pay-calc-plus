4242723901534478d7c7cae0dec521abd0dee577
Grayson <mcclellangg@gmail.com>
2025-09-11 13:02:09 -0400

    refactor load widgets; add tree_view;

--- src/pay_calc_plus/refactor/config.py
+++ src/pay_calc_plus/refactor/config.py
@@ -1,49 +1,47 @@
 TITLE = "Pay-Calc+"
 GEOMETRY = "750x450+250+250"
 
+
 def foo_cmd():
     print("Button clicked!")
+
 
 WIDGETS = {
     "name_label": {
         "type": "label",
-        "coordinates": {"row":0, "column":0, "padx":(20, 8), "pady":(20, 8)},
-        "params": {
-            "text": "Enter employee name : ", "anchor": "w", "width": 18
-        }
+        "coordinates": {"row": 0, "column": 0, "padx": (20, 8), "pady": (20, 8)},
+        "params": {"text": "Enter employee name : ", "anchor": "w", "width": 18},
     },
     "name_entry": {
         "type": "entry",
-        "coordinates": {"row":0, "column":1, "pady":(20, 8)},
-        "params": {"width": 40}
+        "coordinates": {"row": 0, "column": 1, "pady": (20, 8)},
+        "params": {"width": 40},
     },
     "exemptions_label": {
         "type": "label",
         "coordinates": {"row": 1, "column": 0, "padx": (20, 8), "pady": (0, 8)},
-        "params": {
-            "text": "Enter exemptions :  ", "anchor": "w", "width": 18
-        }
+        "params": {"text": "Enter exemptions :  ", "anchor": "w", "width": 18},
     },
     "exemptions_entry": {
         "type": "entry",
         "coordinates": {"row": 1, "column": 1, "pady": (0, 8)},
-        "params": {"width": 40}
+        "params": {"width": 40},
     },
     "gross_label": {
         "type": "label",
         "coordinates": {"row": 2, "column": 0, "padx": (20, 8), "pady": (0, 12)},
-        "params": {
-            "text": "Enter gross pay : ", "anchor": "w", "width": 18
-        }
+        "params": {"text": "Enter gross pay : ", "anchor": "w", "width": 18},
     },
     "gross_entry": {
         "type": "entry",
         "coordinates": {"row": 2, "column": 1, "pady": (0, 12)},
-        "params": {"width": 40}
+        "params": {"width": 40},
     },
     "calc_button": {
         "type": "button",
-        "coordinates": {"row":3, "column":0, "padx":(60, 8), "pady":(0, 8)},
-        "params": {"text": "Calculate", "command": "calculate", "width": 25}
-    }
+        "coordinates": {"row": 3, "column": 0, "padx": (60, 8), "pady": (0, 8)},
+        "params": {"text": "Calculate", "command": "calculate", "width": 25},
+    },
 }
+
+TREEVIEW = {"record_display": {}}
--- src/pay_calc_plus/refactor/ui.py
+++ src/pay_calc_plus/refactor/ui.py
@@ -1,48 +1,66 @@
 """
 Main UI window.
 """
-import tkinter
+
+import tkinter as tk
+from tkinter import ttk
 from config import GEOMETRY, TITLE, WIDGETS
+
 
 class MainWindow:
     def __init__(self):
         # Read data from config and run app
         self.settings = {"title": TITLE, "geometry": GEOMETRY, "widgets": WIDGETS}
-        self.commands = {
-            "calculate": self.calculate_paycheck
-        }
-        self.tk = tkinter.Tk()
+        self.commands = {"calculate": self.calculate_paycheck}
+        self.root = tk.Tk()
         self.setup()
-    
+
     def calculate_paycheck(self):
         """To be imported from separate module"""
         print("calculating paycheck ...")
-    
-    def setup(self):
-        self.tk.title(self.settings["title"])
-        self.tk.geometry(self.settings["geometry"])
-        self.load_widgets()
-    
-    def load_widgets(self):
-        """ 
-        Load widgets from config.
-        """
-        for widget_config in self.settings["widgets"].values():
-            coordinates = widget_config["coordinates"]
-            params = widget_config["params"]
-            if widget_config["type"] == "label":
-                tkinter.Label(self.tk, **params).grid(**coordinates)
 
-            elif widget_config["type"] == "entry":
-                tkinter.Entry(self.tk, **params).grid(**coordinates)
-            
-            elif widget_config["type"] == "button":
+    def setup(self):
+        self.root.title(self.settings["title"])
+        self.root.geometry(self.settings["geometry"])
+        self.load_widgets()
+        self.load_tree_view()
+
+    def load_widgets(self):
+        """
+        Load widgets for parent 'self.root'.
+        """
+        widget_types = {
+            "label": tk.Label,
+            "entry": tk.Entry,
+            "button": tk.Button,
+        }
+        self.widgets = {}
+
+        for name, widget_config in self.settings["widgets"].items():
+            wtype = widget_config["type"]
+            params = widget_config.get("params")
+            parent = self.root
+
+            if wtype == "button":
                 try:
                     cmd_key = params["command"]
                     params["command"] = self.commands[cmd_key]
                 except KeyError as e:
                     print(f"ERROR button requires command: {e}")
-                tkinter.Button(self.tk, **params).grid(**coordinates)
+
+            widget_class = widget_types[wtype]
+            widget = widget_class(parent, **params)
+            self.widgets[name] = widget
+            widget.grid(**widget_config["coordinates"])
+
+    def load_tree_view(self):
+        df = tk.LabelFrame(self.root, text="Paychecks")
+        df.grid(row=4, column=0, columnspan=2, padx=(30, 0))
+        tree = ttk.Treeview(df)
+        tree["columns"] = "Name"
+        print(tree["columns"])
+
+        # tree.pack()
 
     def run(self):
-        self.tk.mainloop()
+        self.root.mainloop()
